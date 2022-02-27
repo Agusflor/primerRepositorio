0aee63b49fc8faa4f397abfc42af5afc17c77c88
Agusflor <agusflormamani@gmail.com>
2022-02-27 02:19:02 -0300

    app de tareas funcional

---
+++ app.js
@@ -0,0 +1,43 @@
+const process = require ('process');
+const {listarTareas,agregarTarea,actualizarTarea,eliminarTarea, filtrarTareas, buscarTarea} = require ('./tareas');
+
+const accion = process.argv[2];
+
+switch (accion) {
+    case 'listar':
+        listarTareas()
+        break;
+    case 'agregar':
+       let descripcion = process.argv[3];
+       if (!descripcion){ 
+        console.log( "Debes indicar un valor");
+        break;
+     }
+       let nuevaTarea = {
+        id: new Date().getTime(),
+        descripcion,
+        estado: 'pendiente'
+    }
+    agregarTarea(nuevaTarea)
+        break;
+    case 'actualizar':
+        actualizarTarea(+process.argv[3])
+        break;
+    case 'eliminar':
+        eliminarTarea(+process.argv[3])
+        break;
+    case 'filtrar':
+        filtrarTareas(process.argv[3])
+        break;
+    case 'buscar':
+        buscarTarea(process.argv[3])
+    break;
+    case undefined:
+        console.log("Debes indicar una accion")
+        break;
+    default:
+        console.log('Acción no permitida');
+        break;
+}
+
+ 
---
+++ tareas.js
@@ -0,0 +1,107 @@
+/*
+const agregarTarea = (tarea)=>{
+    return 'Agregando tarea'
+};
+
+const actualizarTarea = (id) => {
+    return 'Actualizando tarea'
+}
+
+const eliminarTarea =(id)=>{
+    return 'Eliminando tarea'
+}
+
+module.exports = {
+    agregarTarea,
+    actualizarTarea,
+    eliminarTarea
+};*/
+const fs = require('fs');
+const { findSourceMap } = require('module');
+const tareas =require('./tareas.json');//requerir y parsear
+
+const guardarJSON=(tareas) =>{ fs.writeFileSync('./tareas.json', JSON.stringify(tareas,null, 3))
+};
+
+const mostrarInfoTareas = (tareas)=>{
+
+    tareas.forEach((tarea, index) => {
+            console.log(`${index + 1}- ${tarea.descripcion} - estado: ${tarea.estado} - ID: ${tarea.id}`);
+        });
+    return null
+};//for each = ret
+
+
+module.exports={
+
+    listarTareas:()=>{
+        mostrarInfoTareas(tareas)
+        return null
+    },//for each = retorna el objeto , para que la vista sea mas linda podemos parsearlo
+    
+    agregarTarea :(tarea)=> {
+
+    tareas.push(tarea);
+    guardarJSON(tareas)
+    return console.log('Tarea agregada!');    
+
+    },
+    actualizarTarea : (id) => {
+
+        let check = tareas.filter(tarea=>tarea.id===id);
+
+        if(check.length ===0){
+            return console.log("ID inexistente!!")
+        }
+
+       let tareasActualizadas = tareas.map(tarea=>{
+           if ( tarea.id=== id){
+                tarea.estado = 'completado';
+               return tarea}
+              else {
+                  return tarea
+              }}) 
+
+       guardarJSON(tareasActualizadas)
+
+        return console.log( 'Tareas Actualizadas');
+    },
+    eliminarTarea : (id) => {
+
+        let tareasFiltradas = tareas.filter(tarea =>{
+            return tarea.id != id 
+        })
+
+        guardarJSON(tareasFiltradas)
+
+        return console.log('Tarea eliminada'); 
+    },
+    filtrarTareas : (estado) => {
+
+        let estadosValidos = ['pendiente', 'en proceso', 'completado']
+
+        if (!estadosValidos.includes(estado)){
+
+            return console.log('Estado no válido', estadosValidos);
+        }
+
+        let tareasFiltradas = tareas.filter((tarea) => {
+            return tarea.estado === estado;
+        });
+        mostrarInfoTareas(tareasFiltradas);
+        return null;
+    },
+    buscarTarea : (keyword)=>{
+
+        let resultado = tareas.filter((tarea)=>{
+            return tarea.descripcion.toLocaleLowerCase().includes(keyword.toLocaleLowerCase())
+        })//primer elemento y un objeto 
+
+        //filter : devuelve en forma de array, la posicion del array
+
+       mostrarInfoTareas(resultado)
+       return null;
+    }
+}
+
+//información ju
